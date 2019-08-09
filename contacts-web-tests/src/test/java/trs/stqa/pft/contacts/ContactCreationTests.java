0ec34b9ad9cb86a541098ecb2b678550acbed60f
Igor Truskov <itruskov@gmail.com>
2019-08-09 01:42:19 -0400

    import record ( Katalon recorder ) - new contact was created

---
+++ contacts-web-tests/src/test/java/trs/stqa/pft/contacts/ContactCreationTests.java
@@ -0,0 +1,72 @@
+package trs.stqa.pft.contacts;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ContactCreationTests {
+  private WebDriver wd;
+
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testContactCreation() throws Exception {
+    wd.get("http://localhost/addressbook/");
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.id("LoginForm")).submit();
+    wd.findElement(By.linkText("add new")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys("Ivan");
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys("Ivanovich");
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys("Petrov");
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys("21 E Mossovet str");
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys("123456789");
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys("abc@job.com");
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    wd.findElement(By.linkText("home")).click();
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+
+}
