7a06ddaab5951becb9d51c0a42810737d5aba747
Igor Truskov <itruskov@gmail.com>
2019-08-12 12:12:55 -0400

    new imported ContactCreationTest  is OK!

--- addressbook-web-tests/src/test/java/contacts/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/contacts/ContactDeletionTest.java
@@ -5,11 +5,11 @@ import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class ContactDeletionTest  {
+public class ContactDeletionTest {
   private WebDriver wd;
 
 
-  @BeforeMethod(alwaysRun = true)
+  @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -18,21 +18,22 @@ public class ContactDeletionTest  {
   @Test
   public void testContactDeletion() throws Exception {
     wd.get("http://localhost/addressbook/");
-    wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-    wd.findElement(By.cssSelector("img[alt=\"Edit\"]")).click();
-    wd.findElement(By.xpath("(//input[@name='update'])[3]")).click();
+    wd.findElement(By.id("LoginForm")).submit();
+    wd.findElement(By.name("selected[]")).click();
+
+    wd.findElement(By.xpath("//input[@value='Delete']")).click();
+    wd.switchTo().alert().accept();
+    //assertTrue(closeAlertAndGetItsText().matches("^Delete 1 addresses[\\s\\S]$"));
     wd.findElement(By.linkText("home")).click();
   }
 
-  @AfterMethod(alwaysRun = true)
+  @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.quit();
-
   }
 
   private boolean isElementPresent(By by) {
