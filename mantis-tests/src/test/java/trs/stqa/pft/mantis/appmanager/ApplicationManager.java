70843896da727df15e8cb17fffdca3782221b70a
Igor Truskov <itruskov@gmail.com>
2019-09-25 23:58:32 -0400

    8_6 receiving mail on the built-in mail server

--- mantis-tests/src/test/java/trs/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/trs/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -17,10 +17,13 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   private final Properties properties;
-  public WebDriver wd;
+  private WebDriver wd;
 
 
   private String browser;
+  private RegistrationHelper registrationHelper;
+  private FtpHelper ftp;
+  private MailHelper mailHelper;
 
 
   public ApplicationManager(String browser)  {
@@ -32,17 +35,6 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
-
-
-    if(browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    }else if (browser.equals(BrowserType.CHROME)){
-      wd = new ChromeDriver();
-    }else if (browser.equals(BrowserType.IE)){
-      wd = new InternetExplorerDriver();
-    }
-    wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
   }
 
   private boolean isElementPresent(By by) {
@@ -55,14 +47,54 @@ public class ApplicationManager {
   }
 
   public void stop() {
-    wd.quit();
+    if (wd != null){
+      wd.quit();
+    }
   }
 
   public HttpSession newSession(){
     return  new HttpSession(this);
   }
 
-  public Object getProperty(String key) {
+
+
+  public String getProperty(String key) {
     return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null){
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public FtpHelper ftp(){
+    if (ftp == null) {
+      ftp = new FtpHelper(this);
+    }
+    return ftp;
+  }
+
+  public WebDriver getDriver() {
+    if (wd == null){
+      if(browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      }else if (browser.equals(BrowserType.CHROME)){
+        wd = new ChromeDriver();
+      }else if (browser.equals(BrowserType.IE)){
+        wd = new InternetExplorerDriver();
+      }
+      wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
+  }
+
+  public MailHelper mail(){
+    if (mailHelper == null){
+      mailHelper = new MailHelper(this);
+    }
+    return mailHelper;
   }
 }
