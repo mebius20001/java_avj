0b425b823af07ce303fe9920bee31490acdf19ec
Igor Truskov <itruskov@gmail.com>
2019-10-06 18:55:50 -0400

    win8 -> Windiws 8.1

--- addressbook-web-tests/src/test/java/trs/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/trs/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -55,7 +55,7 @@ public class ApplicationManager {
     } else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win8")));
+      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "Windows 8.1")));
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
     wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
